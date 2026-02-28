f81657c7b94ecd7757052d0c7294d58c2f7b9d70
Anish Venkatesalu <anivcs50@gmail.com>
2026-02-28 10:01:20 -0800

    Created Post Endpoint for query

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from data_transfer import DataTransfer
 
 
 app = FastAPI()
 
+class Query(BaseModel):
+    query: str
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/query", response_model=Query)
+async def root(user_query: Query):
+    DataTransfer.send_to_LLM(user_query.query)
+    return user_query
+
---
+++ backend/llm.py
@@ -0,0 +1,5 @@
+from data_transfer import DataTransfer
+
+
+if __name__ == "__main__":
+    DataTransfer.get_query()
