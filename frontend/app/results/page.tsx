cc2b7a4f9604be9131cf6e1d10526c02cc93e0a5
Vaibhav Satish <vaibhavsatish21@hotmail.com>
2026-03-01 00:38:24 -0800

    pushing disanish's changes

--- frontend/app/results/page.tsx
+++ frontend/app/results/page.tsx
@@ -49,14 +49,9 @@ export default function ResultsPage() {
             Results
           </motion.h1>
 
-<<<<<<< Updated upstream
-          <div className="w-full h-[80vh]">
-            <ResultsClient results={results} />
-=======
           {/* Results Client / Map */}
           <div className="w-full h-[80vh]"> {/* Ensure map takes enough height */}
             <ResultsClient results={results.results} />
->>>>>>> Stashed changes
           </div>
         </div>
       </main>
